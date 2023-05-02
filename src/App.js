616b47dcc9e32b9eaefce8716b9d28d3b8379862
Koti-9988 <koti2konduru@gmail.com>
2023-05-02 20:59:58 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name='kkr technologies'
   return (
     <div className="App">
       <header className="App-header">
           
-          <h1>welcome to kkr technologies</h1>
+          <h1>welcome to <h1> {name}</h1></h1>
       </header>
     </div>
   );
